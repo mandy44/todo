18cbb600d68195e897f0e134842de3b0eb941f53
mandy44 <mand@cpan.org>
2013-05-21 20:55:43 +0900

    Make RoR Skelton with MySQL and RSpec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToDo::Application.config.secret_token = '1347f9e20141e606dca5ac0e2296005e3f570e060acaa8fe048d090e9c7fe75b18ea4d9941b8a556033c09af46e809c7402b50d470d1660e5dd59bdb94c288d7'
