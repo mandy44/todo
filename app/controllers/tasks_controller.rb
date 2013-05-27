62beb42050ff443c0d0a1babde85516166905c3e
mandy44 <mand@cpan.org>
2013-05-28 01:11:54 +0900

    delete JSON interface

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,33 +3,18 @@ class TasksController < ApplicationController
   # GET /tasks.json
   def index
     @tasks = Task.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @tasks }
-    end
   end
 
   # GET /tasks/1
   # GET /tasks/1.json
   def show
     @task = Task.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @task }
-    end
   end
 
   # GET /tasks/new
   # GET /tasks/new.json
   def new
     @task = Task.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @task }
-    end
   end
 
   # GET /tasks/1/edit
@@ -42,14 +27,10 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(params[:task])
 
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to root_path }
-        format.json { render json: @task, status: :created, location: @task }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.save
+      redirect_to root_path
+    else
+      render action: "new"
     end
   end
 
@@ -58,14 +39,10 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
 
-    respond_to do |format|
-      if @task.update_attributes(params[:task])
-        format.html { redirect_to root_path }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.update_attributes(params[:task])
+      redirect_to root_path
+    else
+      render action: "edit"
     end
   end
 
@@ -75,10 +52,7 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     @task.destroy
 
-    respond_to do |format|
-      format.html { redirect_to tasks_url }
-      format.json { head :no_content }
-    end
+    redirect_to root_path
   end
 
   # GET /tasks/1/finish
